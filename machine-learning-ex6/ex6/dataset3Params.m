16d3522369456ae52bb3b5f79e8c0152bc536c18
AlessandraMayumi <alessandra_mms@hotmail.com>
2020-12-20 10:30:33 -0300

    SVM

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -23,17 +23,6 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
-% 0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30
-% try all possible pairs of values for C and sigma (e.g., C = 0.3 and sigma = 0.1)
-% 8 2 = 64 different models
-
-##size(X) = 211,2
-##size(y) = 211,1
-##size(Xval) = 200,2
-##size(yval) = 200,1
-
-e = 1000;
-
 Cvalues = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 sigmavalues = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
 for i = 1:8
@@ -41,28 +30,23 @@ for i = 1:8
 
     Ctemp = Cvalues(i);
     sigmatemp = sigmavalues(j);
-    
-    x1 = [1 2 1]; x2 = [0 4 -1];
-    model = svmTrain(Xval, yval, Ctemp, @(x1, x2) gaussianKernel(x1, x2, sigmatemp));
 
-##    x1 = Xval(:,1);
-##    x2 = Xval(:,2);
-##    model = svmTrain(Xval, yval, Ctemp, @(x1, x2) gaussianKernel(x1, x2, sigmatemp));
-    
+    x1 = [1 2 1]; x2 = [0 4 -1];
+    model = svmTrain(X, y, Ctemp, @(x1, x2) gaussianKernel(x1, x2, sigmatemp));
+
     predictions = svmPredict(model, Xval);
 
     errtemp = mean(double(predictions ~= yval));
-    
-    if e > errtemp 
+
+    if e > errtemp || i == 1
       e = errtemp;
       C = Ctemp;
       sigma = sigmatemp;
     endif
-    
+
   endfor
 endfor
 
 % =========================================================================
-C 
-sigma
+
 end
